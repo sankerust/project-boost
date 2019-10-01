03f6ec7c1fcb0f141eee62efcfbc2af4d8643a7e
sanke <egor.tyukin@gmail.com>
2019-10-01 13:51:48 +0300

    add particle effects

---
+++ Assets/Oscillator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour
+{
+  [SerializeField] Vector3 movementVector;
+  
+  float movementFactor; // 0 for not moved, 1 for fully moved
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
